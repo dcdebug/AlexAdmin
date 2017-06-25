b811582854989279dc3fbb5b927fd0105a9faa4b
alex <darrykinger@gmail.com>
2017-06-25 21:41:09 +0800

    AlexAdmin管理后台正式开始开发,PHP版本

---
+++ app/index/controller/Action.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/30
+ * Time: 22:00
+ */
+
+namespace  app\index\controller;
+
+class Action{
+
+    public  function index(){
+        echo "Action";
+    }
+}
